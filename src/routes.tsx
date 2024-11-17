f63080cb19313b4d004870afc73f1eb9a0c49f4c
Nicholas Wilson <nick.wilson@aviture.us.com>
2024-11-17 09:41:44 -0600

    wip start login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,22 @@
 import { createRootRoute, createRoute, createRouter } from "@tanstack/react-router";
 import Gallery from "./gallery/gallery";
+import Login from "./login/login";
 
 export const rootRoute = createRootRoute();
 
 export const galleryRoute = createRoute({
     getParentRoute: () => rootRoute,
-    path: '/',
+    path: '/gallery',
     component: Gallery
 });
 
-const routeTree = rootRoute.addChildren([galleryRoute]);
+export const loginRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: '/login',
+    component: Login
+});
+
+const routeTree = rootRoute.addChildren([galleryRoute, loginRoute]);
 
 export const router = createRouter({ routeTree });
 
