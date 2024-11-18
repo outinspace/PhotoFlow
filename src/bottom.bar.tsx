15e202d580aba6e901f934d100ba5ea9ca18ac23
Nicholas Wilson <nick.wilson@aviture.us.com>
2024-11-18 16:40:55 -0700

    wip

--- src/bottom.bar.tsx
+++ src/bottom.bar.tsx
@@ -1,4 +1,4 @@
-import { Link, useRouterState } from '@tanstack/react-router';
+import { Link, useRouter, useRouterState } from '@tanstack/react-router';
 import React from 'react';
 import { Book, Menu, Search, ViewGrid } from 'iconoir-react';
 
@@ -27,20 +27,21 @@ const options = [
 
 const BottomBar = () => {
     const routerState = useRouterState();
+    const router = useRouter();
 
     return (
         <div className='flex-none flex bg-slate-50 border-t p-2 transition-all'>
             {options.map(option => (
-                <Link
+                <div
                     key={option.route}
-                    to={option.route}
+                    onClick={() => router.navigate({ to: option.route })}
                     className={`flex-auto flex transition-all hover:bg-slate-100 active:bg-slate-300 rounded-md mr-2 last:mr-0 justify-center ${routerState.location.pathname === option.route && 'bg-slate-200 font-bold text-sky-500'}`}
                 >
                     <div className='p-3 flex flex-col items-center text-xs'>
                         <option.icon className='mb-1 h-5' />
                         {option.name}
                     </div>
-                </Link>
+                </div>
             ))}
         </div>
     );
