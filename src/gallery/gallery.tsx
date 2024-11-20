556866b4b110b5c25c1a739fa989863373a0ee36
Nicholas Wilson <nick.wilson@aviture.us.com>
2024-11-20 06:38:46 -0700

    Show processing count at top of gallery

--- src/gallery/gallery.tsx
+++ src/gallery/gallery.tsx
@@ -5,14 +5,28 @@ import { useGallery } from '../queries';
 const Gallery = () => {
     const { data: gallery } = useGallery();
 
-    const sortedItems = useMemo(() => {
-        const items = gallery?.items ?? [];
-        return items.sort((a, b) => a.captureTime < b.captureTime ? 1 : -1); // Date descending
+    const items = gallery?.items ?? [];
+
+    const gridItems = useMemo(() => {
+
+        // Only display items with thumbnails
+        const filteredItems = items.filter(i => i.files.every(f => f.processedVersion !== null));
+
+        return filteredItems.sort((a, b) => a.captureTime < b.captureTime ? 1 : -1); // Date descending
     }, [gallery?.items]);
 
+    const processingItemsCount = items.filter(i => i.files.some(f => f.processedVersion === null)).length;
+
     return (
-        <div className='flex flex-auto overflow-scroll relative'>
-            <ItemGrid items={sortedItems} />
+        <div className='flex flex-auto flex-col overflow-scroll'>
+            {processingItemsCount > 0 && (
+                <div className='p-1 bg-sky-100 text-sky-700 flex justify-center'>
+                    {`${processingItemsCount} photos/videos are being processed.`}
+                </div>
+            )}
+            <div className='flex flex-auto overflow-scroll relative'>
+                <ItemGrid items={gridItems} />
+            </div>
         </div>
     )
 };
