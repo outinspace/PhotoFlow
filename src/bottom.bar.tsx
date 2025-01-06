30f7b0c66d2fd3b0ec189d1f69fbb575ab433782
Nicholas Wilson <nick.wilson@aviture.us.com>
2025-01-05 20:56:55 -0600

    fix: typo

--- src/bottom.bar.tsx
+++ src/bottom.bar.tsx
@@ -40,7 +40,7 @@ const BottomBar = () => {
     const safeAreaPadding = isPwaApp ? 'pb-10 md:pb-2' : '';
 
     return (
-        <div className={'flex-none flex bg-slate-50 border-t p-2 transitin-all ' + safeAreaPadding}>
+        <div className={'flex-none flex bg-slate-50 border-t p-2 transition-all ' + safeAreaPadding}>
             {options.map(option => (
                 <div
                     key={option.route}
