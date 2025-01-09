5246c2eb2457bda99df305338527319a5606f508
Nicholas Wilson <nick.wilson@aviture.us.com>
2025-01-08 22:05:52 -0500

    wip: increase overscan again

--- src/gallery/item.grid.tsx
+++ src/gallery/item.grid.tsx
@@ -64,7 +64,7 @@ const ItemGrid = ({ items: allItems }: Props) => {
         count: rows ?? 0,
         getScrollElement: () => scrollContainerRef.current,
         estimateSize: () => tileSize,
-        overscan: 5,
+        overscan: 15,
         rangeExtractor: useCallback((range: Range) => {
             visibleRangeRef.current = {
                 startIndex: range.startIndex,
