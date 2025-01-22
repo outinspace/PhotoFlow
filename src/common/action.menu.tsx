6d10ff0d25e597f8fd8be8dbc084e43861b1de82
Nicholas Wilson <nick.wilson@aviture.us.com>
2025-01-21 21:42:47 -0600

    feat: Animate action menu

--- src/common/action.menu.tsx
+++ src/common/action.menu.tsx
@@ -1,3 +1,4 @@
+import { animated, useSpring } from '@react-spring/web';
 import React from 'react';
 
 interface Option {
@@ -15,21 +16,54 @@ interface Props {
 }
 
 export const ActionMenu = ({ onDismiss, onActionStarted, position, options }: Props) => {
-
     const optionClasses = 'border-b last:border-none border-slate-200 p-2 bg-slate-50 hover:bg-slate-100 active:bg-slate-200 first:rounded-t last:rounded-b flex items-center';
+
+    const menuSpring = useSpring({
+        from: {
+            y: 20,
+            opacity: 0
+        },
+        to: {
+            y: 0,
+            opacity: 1
+        },
+        config: {
+            duration: 200
+        }
+    });
+
+    const shadowSpring = useSpring({
+        from: {
+            opacity: 0
+        },
+        to: {
+            opacity: 1
+        },
+        config: {
+            duration: 200
+        }
+    });
 
     return (
         <>
-            <div
+            <animated.div
                 className='fixed bg-black/50 top-0 bottom-0 left-0 right-0 z-20'
-                style={{ width: '10000px', height: '10000px', marginLeft: '-5000px', marginTop: '-5000px' }}
+                style={{
+                    width: '10000px',
+                    height: '10000px',
+                    marginLeft: '-5000px',
+                    marginTop: '-5000px',
+                    ...shadowSpring
+
+                }}
                 onClick={() => onDismiss()}
             />
-            <div
-                className={`absolute text-nowrap min-w-40 right-0 text-black mr-2 drop-shadow my-12 z-20`}
+            <animated.div
+                className='absolute right-0 overflow-none z-20 text-nowrap text-black drop-shadow min-w-40'
                 style={{
                     bottom: position === 'top' ? 0 : undefined,
-                    top: position === 'bottom' ? 0 : undefined
+                    top: position === 'bottom' ? 0 : undefined,
+                    ...menuSpring
                 }}
             >
                 {options
@@ -46,7 +80,7 @@ export const ActionMenu = ({ onDismiss, onActionStarted, position, options }: Pr
                             {option.title}
                         </div>
                     ))}
-            </div>
+            </animated.div>
         </>
     )
 }
