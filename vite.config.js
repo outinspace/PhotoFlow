dbc63fd2f9db3927d3c8556357f63752d2320f0b
Nicholas Wilson <nick.wilson@aviture.us.com>
2025-04-05 15:35:04 +0200

    chore: Replace Parcel with Vite

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: 'src/index.html'
+    }
+  },
+  css: {
+    postcss: './.postcssrc'
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  server: {
+    allowedHosts: ['localhost.outin.space']
+  }
+}); 
