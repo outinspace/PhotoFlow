c43b3676462e182cf4ff0f10c4d85c1f883bb31c
Nicholas Wilson <nick.wilson@aviture.us.com>
2025-11-13 21:07:24 +0200

    feat: Change overscan rows

--- src/gallery/item.grid.tsx
+++ src/gallery/item.grid.tsx
@@ -87,19 +87,19 @@ const ItemGrid = ({ items: allItems, albumId, readonly, disableFilteringSorting,
     const zoomLevels = [
         {
             idealTileSize: 40,
-            overscanRows: 20
+            overscanRows: 5
         },
         {
             idealTileSize: 50,
-            overscanRows: 20
+            overscanRows: 10
         },
         {
             idealTileSize: 70,
-            overscanRows: 30
+            overscanRows: 20
         },
         {
             idealTileSize: 110,
-            overscanRows: 40
+            overscanRows: 30
         },
         {
             idealTileSize: Math.min(containerWidth, 300),
