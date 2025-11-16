55a68adee6197e3ac2b535595fc569ded6aa2dd7
Nicholas Wilson <nick.wilson@aviture.us.com>
2025-11-16 20:23:54 +0200

    feat: Show live photo on long press

--- src/gallery/item.media.tsx
+++ src/gallery/item.media.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Item } from '../types';
 import { nonSelectable } from '../styles';
+import { useLongPress } from 'use-long-press';
 
 const zIndex = {
     controls: 10,
@@ -30,10 +31,14 @@ const ItemMedia = ({ item, isPrimary }: Props) => {
         }
     }, [isPrimary, videoRef]);
 
+    const longPressHandlers = useLongPress(() => {
+        setShowLivePhoto(true);
+    });
+
     return (
         <div
             className={`fixed top-0 bottom-0 left-0 right-0 ${isLivePhoto && showLivePhoto && 'animate-[pulse_0.5s_ease-in-out_1]'}`}
-            onDoubleClick={() => setShowLivePhoto(true)}
+            {...longPressHandlers()}
         >
             {imageFile && <>
                 <img
